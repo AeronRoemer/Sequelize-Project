28a83f30dc98ccecefbbf20973dc87d61db6fa3a
Aeron Roemer <57688724+AeronRoemer@users.noreply.github.com>
2020-04-03 18:20:23 -0600

    new columns

--- app.js
+++ app.js
@@ -6,9 +6,20 @@ const { Movie } = db.models;
         await db.sequelize.sync({ force: true });
   try {
     const movie = await Movie.create({
-        title: 'Toy Story',})
-    const movie2 = await Movie.create({
-            title: 'Star Wars',});
+        title: 'Toy Story',
+        runtime: 81,
+        releaseDate: '1995-11-22',
+        isAvailableOnVHS: true,
+      });
+      console.log(movie.toJSON());
+  
+      const movie2 = await Movie.create({
+        title: 'The Incredibles',
+        runtime: 115,
+        releaseDate: '2004-04-14',
+        isAvailableOnVHS: true,
+      });
+      console.log(movie2.toJSON());  
   } catch(error){
     console.error("Database error: ", error)
   }
--- db/models/movie.js
+++ db/models/movie.js
@@ -3,7 +3,26 @@ const Sequelize = require('sequelize');
 module.exports = (sequelize) => {
   class Movie extends Sequelize.Model {}
   Movie.init({
-    title: Sequelize.STRING,
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false, // disallow null
+    },
+      //Sequelize.STRING is 255 (or varchar(255)), but you can specify a different length after the type. For example, Sequelize.STRING(500)
+    runtime: {
+        type: Sequelize.INTEGER
+    },
+    releaseDate: {
+        type: Sequelize.DATEONLY
+    }, // DATE data type accepts a date value provided in yyyy-mm-dd hh:mm:ss format, while DATEONLY accepts a date value in yyyy-mm-dd format (DATE without time).
+    isAvailableOnVHS: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+    },
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true, //sets value as primary key value
+        autoIncrement: true //auto increments on each new row
+    }
   }, { sequelize });
 
   return Movie;
