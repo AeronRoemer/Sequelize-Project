aac0b8f0b94b961b86de257d7a2a967c643cd032
Aeron Roemer <57688724+AeronRoemer@users.noreply.github.com>
2020-04-03 18:00:08 -0600

    Reorganized files

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-const Sequelize = require('sequelize');
+const db = require('./db');
+const { Movie } = db.models;
 
-const sequelize = new Sequelize({
-    //which version of SQL is being used in the database
-    dialect: 'sqlite',
-    //specifies file path for database. Creates a movies.db
-    storage: 'movies.db'  
-});
 
-// IIFE (Immediately Invoked Function Expression) created: (function)();
-//run via npm start. Console should show 'connection success' and a new movies.db file should appear
-(async ()=> {
-  try{
-    await sequelize.authenticate();
-    console.log("connection success")
-  }catch(error){
+(async () => {
+        await db.sequelize.sync({ force: true });
+  try {
+    const movie = await Movie.create({
+        title: 'Toy Story',})
+    const movie2 = await Movie.create({
+            title: 'Star Wars',});
+  } catch(error){
     console.error("Database error: ", error)
   }
 })();
+
+  
---
+++ db/index.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'movies.db',
+  logging: false
+});
+
+const db = {
+    //sequelize instance containing diablect etc
+  sequelize,
+    //sequelize module from 'require'
+  Sequelize,
+    //movie DB modeal
+  models: {},
+};
+
+db.models.Movie = require('./models/movie.js')(sequelize);
+
+module.exports = db;
